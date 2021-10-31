41bc67539b2b65f423046af44ac561a79c628578
barneygale <barney.gale@gmail.com>
2021-10-31 19:47:58 +0000

    Mouse: add `move()`, `hold()`, `click()`, etc methods.

--- asyncvnc/mouse.py
+++ asyncvnc/mouse.py
@@ -1,4 +1,5 @@
 from asyncio import StreamWriter
+from contextlib import contextmanager
 from dataclasses import dataclass, field
 from enum import Flag
 
@@ -34,13 +35,43 @@ class Mouse:
     """
 
     writer: StreamWriter = field(repr=False)
+    _buttons: MouseButton = MouseButton.NONE
+    _x: int = 0
+    _y: int = 0
 
-    def update(self, buttons: MouseButton, x: int, y: int):
-        """
-        Sends a mouse update to the server.
-        """
-
+    def _update(self):
         self.writer.write(b'\x05')
-        self.writer.write(buttons.value.to_bytes(1, 'big'))
-        self.writer.write(x.to_bytes(2, 'big'))
-        self.writer.write(y.to_bytes(2, 'big'))
+        self.writer.write(self._buttons.value.to_bytes(1, 'big'))
+        self.writer.write(self._x.to_bytes(2, 'big'))
+        self.writer.write(self._y.to_bytes(2, 'big'))
+
+    def move(self, x: int, y: int):
+        self._x = x
+        self._y = y
+        self._update()
+
+    @contextmanager
+    def hold(self, button: MouseButton = MouseButton.LEFT):
+        self._buttons |= button
+        self._update()
+        try:
+            yield
+        finally:
+            self._buttons &= ~button
+            self._update()
+
+    def click(self, button: MouseButton = MouseButton.LEFT):
+        with self.hold(button):
+            pass
+
+    def middle_click(self):
+        self.click(MouseButton.MIDDLE)
+
+    def right_click(self):
+        self.click(MouseButton.RIGHT)
+
+    def scroll_up(self):
+        self.click(MouseButton.SCROLL_UP)
+
+    def scroll_down(self):
+        self.click(MouseButton.SCROLL_DOWN)
